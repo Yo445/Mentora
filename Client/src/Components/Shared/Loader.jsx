4bb908c2a86323cc6fc7036f61dd3f50bc94d906
Yo445 <ya108942@gmail.com>
2024-09-17 12:08:48 +0300

    add login

--- Client/src/Components/Shared/Loader.jsx
+++ Client/src/Components/Shared/Loader.jsx
@@ -4,8 +4,8 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 z-10 flex justify-center items-center min-h-screen">
       <div className="flex-col gap-4 w-full flex items-center justify-center">
-        <div className="w-28 h-28 border-8 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-[#ddff7d] rounded-full">
-          <div className="w-20 h-20 border-8 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-[black] rounded-full" />
+        <div className="w-28 h-28 border-8 border-transparent text-[#ddff7d]text-4xl animate-spin flex items-center justify-center border-t-[#ddff7d] rounded-full">
+          <div className="w-20 h-20 border-8 border-transparent text-[black] text-2xl animate-spin flex items-center justify-center border-t-[black] rounded-full" />
         </div>
       </div>
     </div>
