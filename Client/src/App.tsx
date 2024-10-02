8e95ef29965f9261466648e515009f23bcbb2c5c
Yo445 <ya108942@gmail.com>
2024-10-03 00:55:51 +0300

    solve more problems

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App(): JSX.Element {
 
-  return <Outlet />
+  return (
+    <GoogleOAuthProvider clientId='28611108443-fuc05t4db416chirpiuif3inn9kjmumn.apps.googleusercontent.com'>
+      <Outlet />
+    </GoogleOAuthProvider>
+
+  );
+  
+  // 
 }
