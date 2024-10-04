e6e9b0a37de7ac2c355d248f89d15a809203248f
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-10-04 14:41:45 +0300

    commit

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import 'dotenv/config';
+import { Outlet } from 'react-router-dom';
+
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 export default function App(): JSX.Element {
 
   return (
-    <GoogleOAuthProvider clientId='28611108443-fuc05t4db416chirpiuif3inn9kjmumn.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <Outlet />
     </GoogleOAuthProvider>
 
